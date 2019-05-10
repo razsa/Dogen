013d21b59f717740e1cdffa579b2a6bed39bbd5f
razsa <21493e@gmail.com>
2019-05-10 02:19:19 -0400

    Optimized final code

--- code/app.py
+++ code/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -13,19 +13,30 @@ jwt = JWT(app, authenticate, identity)
 items = []
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type = float,
+        required = True,
+        help = "This field cannot be left blank!"
+    )
+
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if item else 404
 
     def post(self, name):
+        
         if next(filter(lambda x: x['name'] == name, items), None):
             return {'message': "An item with name '{}' already exists.".format(name)}, 400
-        
+
+        data = Item.parser.parse_args()
+
         data = request.get_json()
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
+        
 
     def delete(self, name):
         global items
@@ -33,7 +44,8 @@ class Item(Resource):
         return {'message': 'Item deleted'}
 
     def put(self, name):
-        data = request.get_json()
+        data = Item.parser.parse_args()
+       
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
             item = {'name': name, 'price': data['price']}
@@ -42,7 +54,6 @@ class Item(Resource):
             item.update(data)
         return item
 
-
 class ItemList(Resource):
     def get(self):
         return {'items': items}
@@ -50,6 +61,8 @@ class ItemList(Resource):
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 
-app.run(port=5000, debug=True)
+app.run(port = 5000, debug = True)
+
+
 
 
